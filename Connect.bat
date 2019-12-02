bfbe2e03032e7f7381145ec80756694fd51b769d
Peter Cheung <takshing2001@hotmail.com>
2019-12-02 15:10:45 +0800

    Update Connect.bat
    ADD USERNAME WRONG

--- Connect.bat
+++ Connect.bat
@@ -1,18 +1,24 @@
 @echo off
 echo "Initializing... "
 echo "Testing Network Environment..."
-ping Your Server >null
+ping YOURDOMAINSERVER >null
+DEL NULL
 IF ERRORLEVEL 1 goto error
 IF ERRORLEVEL 0 goto OK
 :error
-echo msgbox "Sorry! Your are not connecting  Domain Network, Please Check your  Wifi Setting" > %tmp%\popup.vbs
+echo msgbox "Sorry! Your are not connecting  DOMAIN  Network, Please Check your  Wifi Setting" > %tmp%\popup.vbs
 cscript /nologo %tmp%\popup.vbs
 del %tmp%\tmp.vbs
 exit 0
 :OK
-NET USE Your Server /user:domain\domainuser domainuserpassword
-echo msgbox "Your Connected Domain Network , Now you can use Domain Printer" > %tmp%\popup.vbs
+NET USE \\YOURDOMAINSERVER /user: DOMAIN \DOMAINUSER DOMAINUSERPASSWORD
+IF ERRORLEVEL 1 goto username
+echo msgbox "Your Connected DOMAIN Network , Now you can use DOMAIN Printer" > %tmp%\popup.vbs
 cscript /nologo %tmp%\popup.vbs
 del %tmp%\tmp.vbs
 exit 0
-
+:username
+echo msgbox "Please contact LOCAL IT to check your account status" > %tmp%\popup.vbs
+cscript /nologo %tmp%\popup.vbs
+del %tmp%\tmp.vbs
+exit 0
